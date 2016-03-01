ee559a584f9f3d3610684627f994d0e38e365dce
Alief <alief@berrybenka.com>
2016-03-01 18:31:27 +0700

    nothing

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -1,9 +1,9 @@
 <ul class="sidebar-menu">
-  <li class="<?php echo ($page == "Dashboard") ? "active" : "" ; ?>"><a href="<?php echo base_url() ?>dashboard/"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-  <li class="<?php echo ($page == "Admin") ? "active" : "" ; ?>"><a href="<?php echo base_url() ?>admin/"><i class="fa fa-user"></i> <span>Admin</span></a></li>
-  <li class="<?php echo ($page == "Slider") ? "active" : "" ; ?>"><a href="<?php echo base_url() ?>slider/"><i class="fa fa-object-group"></i> <span>Slider</span></a></li>
-  <li class="<?php echo ($page == "Color") ? "active" : "" ; ?>"><a href="<?php echo base_url() ?>color/"><i class="fa fa-tint"></i> <span>Color</span></a></li>
-  <li class="<?php echo ($page == "Category") ? "active" : "" ; ?>"><a href="<?php echo base_url() ?>category/"><i class="fa fa-bell"></i> <span>Category</span></a></li>
-  <li class="<?php echo ($page == "Brand") ? "active" : "" ; ?>"><a href="<?php echo base_url() ?>brand/"><i class="fa fa-leaf"></i> <span>Brand</span></a></li>
-  <li class="<?php echo ($page == "Products") ? "active" : "" ; ?>"><a href="<?php echo base_url() ?>products/"><i class="fa fa-gavel"></i> <span>Products</span></a></li>
+  <li class="<?php echo ($page == "Dashboard") ? "active" : ""; ?>"><a href="<?php echo base_url() ?>dashboard/"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+  <li class="<?php echo ($page == "Admin") ? "active" : ""; ?>"><a href="<?php echo base_url() ?>admin/"><i class="fa fa-user"></i> <span>Admin</span></a></li>
+  <li class="<?php echo ($page == "Slider") ? "active" : ""; ?>"><a href="<?php echo base_url() ?>slider/"><i class="fa fa-object-group"></i> <span>Slider</span></a></li>
+  <li class="<?php echo ($page == "Color") ? "active" : ""; ?>"><a href="<?php echo base_url() ?>color/"><i class="fa fa-tint"></i> <span>Color</span></a></li>
+  <li class="<?php echo ($page == "Category") ? "active" : ""; ?>"><a href="<?php echo base_url() ?>category/"><i class="fa fa-bell"></i> <span>Category</span></a></li>
+  <li class="<?php echo ($page == "Brand") ? "active" : ""; ?>"><a href="<?php echo base_url() ?>brand/"><i class="fa fa-leaf"></i> <span>Brand</span></a></li>
+  <li class="<?php echo ($page == "Products") ? "active" : ""; ?>"><a href="<?php echo base_url() ?>products/"><i class="fa fa-gavel"></i> <span>Products</span></a></li>
 </ul>
